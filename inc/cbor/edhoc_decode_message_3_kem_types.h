e1a693c217e84893d0143ef2fd5b2f025a81bdbc
ehalep <ehalep@gmail.com>
2025-07-19 10:56:07 +0000

    New message types for the KEM-based authentication for EDHOC. All types and function calls.

---
+++ inc/cbor/edhoc_decode_message_3_kem_types.h
@@ -0,0 +1,37 @@
+/*
+ * Generated using zcbor version 0.8.99
+ * https://github.com/NordicSemiconductor/zcbor
+ * Generated with a --default-max-qty of 3
+ */
+
+#ifndef EDHOC_DECODE_MESSAGE_3_KEM_TYPES_H__
+#define EDHOC_DECODE_MESSAGE_3_KEM_TYPES_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Which value for --default-max-qty this file was created with.
+ *
+ *  The define is used in the other generated file to do a build-time
+ *  compatibility check.
+ *
+ *  See `zcbor --help` for more information about --default-max-qty
+ */
+#define DEFAULT_MAX_QTY 3
+
+struct message_3_kem {
+	struct zcbor_string ct_R;
+	struct zcbor_string CIPHERTEXT_3;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EDHOC_DECODE_MESSAGE_3_KEM_TYPES_H__ */
---
+++ inc/cbor/edhoc_encode_message_4_kem_types.h
@@ -0,0 +1,37 @@
+/*
+ * Generated using zcbor version 0.8.99
+ * https://github.com/NordicSemiconductor/zcbor
+ * Generated with a --default-max-qty of 3
+ */
+
+#ifndef EDHOC_ENCODE_MESSAGE_4_KEM_TYPES_H__
+#define EDHOC_ENCODE_MESSAGE_4_KEM_TYPES_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Which value for --default-max-qty this file was created with.
+ *
+ *  The define is used in the other generated file to do a build-time
+ *  compatibility check.
+ *
+ *  See `zcbor --help` for more information about --default-max-qty
+ */
+#define DEFAULT_MAX_QTY 3
+
+struct message_4_kem {
+	struct zcbor_string ct_I;
+	struct zcbor_string CIPHERTEXT_4;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EDHOC_ENCODE_MESSAGE_4_KEM_TYPES_H__ */
